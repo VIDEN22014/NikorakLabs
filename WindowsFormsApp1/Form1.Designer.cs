6fad0d3c51bdfdd8c9dc7b35d587a2f8d2376dda
Bodgan Humenyk <dxbogdan4@gmail.com>
2020-11-01 18:11:51 +0200

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть К-сть Ітерацій: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(174, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(280, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Побудувати Фрактал";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picture;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static PictureBox mainPictureBox = new PictureBox();
+        static Bitmap bmp = new Bitmap(10000, 10000);
+        static Graphics graph = Graphics.FromImage(bmp);
+        static Pen pen = new Pen(Color.Black);
+        static CantorRectangleGroup group1 = new CantorRectangleGroup();
+        static TextBox mainTextBox = new TextBox();
+        static int iter;
+        public Form1()
+        {
+            InitializeComponent();
+            mainPictureBox = pictureBox;
+            mainTextBox = textBox1;
+        }
+        int TextBoxToInt(TextBox textBox)
+        {
+            if (textBox.Text == "") { return 0; }
+            return Convert.ToInt32(textBox.Text);
+        }
+        void CantorFractal(int iteration)
+        {
+            if (iteration == iter) { return; }
+            for (int i = 0; i < Math.Pow(2, iteration); i++)
+            {
+                AddRectangle(group1.group[0]);
+            }
+            CantorFractal(iteration + 1);
+        }
+        void AddRectangle(CantorRectangle rect)
+        {
+            CantorRectangle temp1 = new CantorRectangle(rect.x, rect.y + 20, rect.width / 3.0f);
+            CantorRectangle temp2 = new CantorRectangle(rect.x + 2 / 3.0f * rect.width, rect.y + 20, rect.width / 3.0f);
+            group1 += temp1;
+            group1 += temp2;
+            group1 -= rect;
+            group1.group[group1.group.Length - 1].DrawRectangle();
+            group1.group[group1.group.Length - 2].DrawRectangle();
+        }
+        class CantorRectangle
+        {
+            public float x { get; } = 0;
+            public float y { get; } = 0;
+            public float width { get; } = 0;
+            public CantorRectangle(float x, float y, float width)
+            {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+            }
+            public void DrawRectangle()
+            {
+                graph.DrawRectangle(pen, x + 10, y + 10, width, 10);
+                mainPictureBox.Image = bmp;
+            }
+        }
+        class CantorRectangleGroup
+        {
+            public CantorRectangle[] group;
+            public CantorRectangleGroup()
+            {
+                group = new CantorRectangle[0];
+            }
+            public static CantorRectangleGroup operator +(CantorRectangleGroup gr, CantorRectangle rect)
+            {
+                Array.Resize(ref gr.group, gr.group.Length + 1);
+                gr.group[gr.group.Length - 1] = rect;
+                return gr;
+            }
+            public static CantorRectangleGroup operator -(CantorRectangleGroup gr, CantorRectangle rect)
+            {
+                bool isFind = false;
+                for (int i = 0; i < gr.group.Length; i++)
+                {
+                    if (isFind)
+                    {
+                        CantorRectangle temp = gr.group[i];
+                        gr.group[i] = gr.group[i - 1];
+                        gr.group[i - 1] = temp;
+                    }
+                    if (gr.group[i] == rect)
+                    {
+                        isFind = true;
+                    }
+                }
+                if (isFind)
+                {
+                    Array.Resize(ref gr.group, gr.group.Length - 1);
+                }
+                return gr;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            graph.Clear(Color.Transparent);
+            CantorRectangle rect1 = new CantorRectangle(0, 30, 1200);
+            iter = TextBoxToInt(mainTextBox);
+            foreach (CantorRectangle i in group1.group)
+            {
+                group1 -= i;
+            }
+            foreach (CantorRectangle i in group1.group)
+            {
+                group1 -= i;
+            }
+            group1 += rect1;
+            group1.group[0].DrawRectangle();
+            CantorFractal(0);
+        }
+    }
+}
